a900fa9a3db8398fa58a983760318f3457527bb3
bilal-rashid <bilalrashid1992@gmail.com>
2025-06-06 03:34:42 +0500

    minor fixes and favicon icon change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link 
+          rel="icon" 
+          href="https://cdn.prod.website-files.com/5edfb37892e6a635a29ae550/5f1548a03cd0e652c1e0e7de_favicon-32x32.png" 
+          type="image/png"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,14 +26,15 @@ export default function Home() {
       
       <div className={`${geistSans.className} ${geistMono.className} min-h-screen font-[family-name:var(--font-geist-sans)] relative overflow-auto h-screen`}>
 
-        
-        <main className="px-4 sm:px-8 md:px-16 pb-10">
-          {loading && (
-            <div className="flex justify-center items-center py-20">
-              <LoadingView/> 
-              <span className="ml-3 text-white">Loading users...</span>
+        {loading && (
+            <div className="w-screen h-screen bg-white/30 flex flex-col flex-nowrap justify-center items-center">
+              <div className="flex flex-col">
+                <LoadingView/>
+                <span className="ml-3 text-white">Loading users...</span>
+              </div>
             </div>
-          )}
+        )}
+        <main className="px-4 sm:px-8 md:px-16 pb-10 flex flex-col flex-nowrap justify-center items-center">
           
           {error && (
             <div className="text-red-500 text-center p-4 bg-red-50 rounded-lg max-w-2xl mx-auto">
@@ -42,7 +43,7 @@ export default function Home() {
           )}
           
           {!loading && !error && (
-            <div className="columns-1 gap-6 sm:columns-2 xl:columns-3 2xl:columns-4 space-y-6">
+            <div className="columns-1 gap-6 sm:columns-2 xl:columns-3 space-y-6 max-w-7xl">
               {users.map((user) => (
                 <div key={user.id} className="break-inside-avoid mb-6">
                   <UserCard user={user} onViewMore={handleViewMore} />
